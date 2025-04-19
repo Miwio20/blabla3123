092ed8181e86e34f66ed004f35990a41f918c3ef
Miwio <126167081+Miwio20@users.noreply.github.com>
2025-04-19 20:21:07 +0300

    Add files via upload

---
+++ dzhzzbivsa.py
@@ -0,0 +1,7 @@
+try:
+    user_input = input("Введіть число: ")
+    number = int(user_input)
+    print("Ви ввели ціле число:", number)
+except ValueError:
+    print("Помилка: введене значення не можна конвертувати в ціле число.")
+3232
